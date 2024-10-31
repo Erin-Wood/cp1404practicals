2fc636d531cd664ad6103f9ab8807a0d0760ff7a
ErinW <erin.wood@my.jcu.edu.au>
2024-10-31 14:12:02 +1000

    Complete language.py

--- prac_06/language.py
+++ prac_06/language.py
@@ -1,6 +1,8 @@
 """
 Module: languages.py
-Estimated time to complete: 15 minutes
+Estimated time to complete: 30 minutes
+Current time: 2pm
+Time to complete: 45 minutes
 """
 
 from programming_language import ProgrammingLanguage
@@ -9,14 +11,11 @@ python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
 ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
 visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 
-# Print the ProgrammingLanguage instances to verify the __str__ method
 print(python)
 print(ruby)
 print(visual_basic)
 
-# Create a list containing the ProgrammingLanguage objects
 languages_list = [python, ruby, visual_basic]
 
-# Print the list to confirm it contains the objects
 for language in languages_list:
     print(language)
