4b5d304298134766b373c3c89cbaf9c0df1ddad8
ErinW <erin.wood@my.jcu.edu.au>
2024-11-03 13:22:12 +1000

    Added guitars.py

---
+++ prac_06/guitars.py
@@ -0,0 +1,29 @@
+"""
+guitars.py
+This program allows the user to enter multiple guitars and displays each guitar's details.
+"""
+
+from guitar import Guitar
+
+
+def main():
+    print("My guitars!")
+    guitars = []
+
+    name = input("Name: ")
+    while name:
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(f"{guitar} added.")
+
+        name = input("Name: ")
+
+    print("\nThese are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = " (vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+
+main()
